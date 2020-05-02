f390ffabef970d4c0eaf5febb6db0d07fe00662b
YXu <43019609+YXu42@users.noreply.github.com>
2020-05-02 17:53:08 -0400

    Update RandomFunctions.py

--- RandomFunctions.py
+++ RandomFunctions.py
@@ -2,6 +2,7 @@ import random
 
 INFECTION_RADIUS = 6
 CHANCE_OF_INFECTION = 20
+PEOPLE_INFECTED = []
 
 # when we put the code together remember to make an if that checks if person is already True. efficiecny
 def is_infected(position1, position2):
@@ -24,6 +25,7 @@ person2 = Person(False, [49, 48])
 
 if is_infected(person1.position, person2.position):
     person1.infected = True
+    PEOPLE_INFECTED.append(person1)
 
 print(person1.infected)
 
