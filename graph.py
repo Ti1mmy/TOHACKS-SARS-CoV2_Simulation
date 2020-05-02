b34aea6a6cdc28e553ea57259baaa4f1eea9060f
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2020-05-02 15:25:38 -0400

    start graph

--- graph.py
+++ graph.py
@@ -7,10 +7,6 @@ HEIGHT = 480
 mouse_x = 0
 mouse_y = 0
 mouse_press = False
-ball_color = arcade.color.BLUE
-slider_x = WIDTH / 2
-slider_y = HEIGHT / 2
-slide_color = arcade.color.BLUE
 
 
 def setup():
@@ -31,32 +27,12 @@ def setup():
 
 
 def update(delta_time):
-    global ball_color, slide_color, slider_x
-    if mouse_press:
-        ball_color = arcade.color.RED
-    else:
-        ball_color = arcade.color.BLUE
-    if slider_x - 5 <= mouse_x <= slider_x + 5 and slider_y - 13 <= mouse_y <= slider_y + 13:
-        slide_color = arcade.color.DARK_GRAY
-        if mouse_press:
-            slide_color = arcade.color.GRAY
-            if WIDTH/2 - 100 < slider_x < WIDTH/2 + 100:
-                slider_x = mouse_x
-            if slider_x < 420 or slider_x > 220:
-                if 220 <= mouse_x <= 420:
-                    slider_x = mouse_x
-    else:
-        slide_color = arcade.color.BLUE
 
 
 def on_draw():
     arcade.start_render()
     # Draw in here...
     # arcade.draw_circle_filled(mouse_x, mouse_y, 25, ball_color)
-    arcade.draw_text(f'mouse_x={mouse_x}\nmouse_y={mouse_y}\nmouse_press={mouse_press}\nslider_x={slider_x}', 0, 0, arcade.color.BLACK)
-    arcade.draw_rectangle_outline(WIDTH/2, HEIGHT/2, 200, 5, arcade.color.BLACK)
-    arcade.draw_rectangle_filled(slider_x, slider_y, 10, 25, slide_color)
-    arcade.draw_text(f'{((slider_x-220) // 2):.2f}%', WIDTH/2 - 20, HEIGHT/2 - 30, arcade.color.BLACK)
 
 
 def on_key_press(key, modifiers):
