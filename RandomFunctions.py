afe12bb9b2273f2c1271290ffab0acdf36cea974
YXu <43019609+YXu42@users.noreply.github.com>
2020-05-02 17:15:45 -0400

    Update RandomFunctions.py

--- RandomFunctions.py
+++ RandomFunctions.py
@@ -1,18 +1,31 @@
-
 import random
 
 INFECTION_RADIUS = 6
 CHANCE_OF_INFECTION = 20
 
-# basically if its in the square it ll run the probability thingy to see if infected
+
 def is_infected(position1, position2):
-    if (position2[0] - INFECTION_RADIUS <= position1[0] and position1[0] <= position2[0] + INFECTION_RADIUS) or (position2[1] - INFECTION_RADIUS <= position1[1] and position1[1] <= position2[1] + INFECTION_RADIUS):
-        return random.randrange(100) <= CHANCE_OF_INFECTION
+    if (position2[0] - INFECTION_RADIUS <= position1[0] and position1[0] <= position2[0] + INFECTION_RADIUS) and (position2[1] - INFECTION_RADIUS <= position1[1] and position1[1] <= position2[1] + INFECTION_RADIUS):
+        return random.randrange(100) < CHANCE_OF_INFECTION
+    else:
+        return False
 
 class Person:
-    infected = False #technically we can just create each object with this already filled out right?
+    infected = False
     position = []
 
     def __init__(self, infected, position):
         self.infected = infected
         self.position = position
+
+    
+person1 = Person(False, [50, 50])
+person2 = Person(False, [49, 48])
+
+if is_infected(person1.position, person2.position):
+    person1.infected = True
+
+print(person1.infected)
+
+
+    
